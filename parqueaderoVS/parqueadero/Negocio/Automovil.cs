1d1989c4f5f94948bad502ce40b839798d0cc7d2
carmhernadez <carmencita1284@hotmail.com>
2014-09-07 18:55:59 -0500

    subiendo mis primeros archivos

---
+++ parqueaderoVS/parqueadero/Negocio/Automovil.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace parqueaderoVS.parqueadero.Negocio
+{
+    public class Automovil
+    {
+    }
+}
